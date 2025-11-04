7d64e55c266188fc244a484b4aaf26861573212f
Zi-guo <ziyang.guo.jg@gmail.com>
2025-11-03 18:00:31 -0800

    Create 6Qel5FUgl9C_captions.js

---
+++ IST699/Problem Based learning/story_content/6Qel5FUgl9C_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/6Qel5FUgl9C_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.105.35604.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:01.411%0D%0APush%20the%20next%20button%20when%20ready.%0D%0A%0D%0A"}]})
